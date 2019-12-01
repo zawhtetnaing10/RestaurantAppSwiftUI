937d7276b5966f477f954ea556ac0d5117f257ae
zawhtetnaing10 <zawgyi.gog@gmail.com>
2019-12-01 12:36:07 +0630

    Initial Commit

---
+++ RestaurantApp/RestaurantApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestaurantApp
+//
+//  Created by Zaw Htet Naing on 01/12/2019.
+//  Copyright © 2019 Zaw Htet Naing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
